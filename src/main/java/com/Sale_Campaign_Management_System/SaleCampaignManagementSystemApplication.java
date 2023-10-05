86f68e323d7965dfc89125da9bc1fbcb06eead20
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-10-05 11:02:14 +0530

    Initial commit

---
+++ src/main/java/com/Sale_Campaign_Management_System/SaleCampaignManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Sale_Campaign_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleCampaignManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleCampaignManagementSystemApplication.class, args);
+	}
+
+}
